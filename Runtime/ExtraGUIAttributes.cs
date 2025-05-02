ea7be926d8833a323bd392df99979838f0dc4738
Michal Vrba <michal.vrba@promethist.ai>
2025-05-02 13:50:28 +0200

    Fix attributes for Unity 2022 compatibility

--- Runtime/ExtraGUIAttributes.cs
+++ Runtime/ExtraGUIAttributes.cs
@@ -1,20 +1,20 @@
 ﻿using UnityEngine;
+using System;
 
 namespace ExtraGUIs.Editor
 {
     public class UseExtraGUIDrawer : PropertyAttribute
     { }
-
-    // TODO Maybe [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Field, Inherited = true)]
     public class ContainsReferences : PropertyAttribute
     {
-        public ContainsReferences() : base(true)
+        public ContainsReferences() : base()
         { }
     }
-    
+    [AttributeUsage(AttributeTargets.Field, Inherited = true)]
     public class ReferenceList : PropertyAttribute
     {
-        public ReferenceList() : base(true)
+        public ReferenceList() : base()
         { }
     }
 }
